bd33b8e323e6c75d9da930256e5e7d167350267d
jamestang12 <homing.tang@ryerson.ca>
2020-04-21 00:27:47 -0400

    UI Function Created

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from "react";
-import uuid from "uuid";
+import { v4 as uuidv4 } from "uuid";
 import ContactContext from "./contactContext";
 import contactReducer from "./contactReducer";
 import {
@@ -39,16 +39,30 @@ const ContactState = (props) => {
         type: "professional",
       },
     ],
+    current: null,
   };
   const [state, dispatch] = useReducer(contactReducer, initialState);
 
   //Add Contact
+  const addContact = (contact) => {
+    contact.id = uuidv4();
+    dispatch({ type: ADD_CONTACT, payload: contact });
+  };
 
   //Delete Contact
+  const deleteContact = (id) => {
+    dispatch({ type: DELETE_CONTACT, payload: id });
+  };
 
   //Set Current Contact
+  const setCurrent = (contact) => {
+    dispatch({ type: SET_CURRENT, payload: contact });
+  };
 
   //Clear Current Contact
+  const clearCurrent = () => {
+    dispatch({ type: CLEAR_CURRENT });
+  };
 
   //Update Contact
 
@@ -60,6 +74,11 @@ const ContactState = (props) => {
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
+        current: state.current,
+        addContact,
+        deleteContact,
+        clearCurrent,
+        setCurrent,
       }}
     >
       {props.children}
---
+++ client/src/context/contact/contactReducer.js
@@ -0,0 +1,40 @@
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_ALERT,
+  CLEAR_CURRENT,
+  SET_CURRENT,
+  CLEAR_FILTER,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  REMOVE_ALERT,
+} from "../types";
+
+export default (state, action) => {
+  switch (action.type) {
+    case ADD_CONTACT:
+      return {
+        ...state,
+        contacts: [...state.contacts, action.payload],
+      };
+    case DELETE_CONTACT:
+      return {
+        ...state,
+        contacts: state.contacts.filter(
+          (contact) => contact.id !== action.payload
+        ),
+      };
+    case SET_CURRENT:
+      return {
+        ...state,
+        current: action.payload,
+      };
+    case CLEAR_CURRENT:
+      return {
+        ...state,
+        current: null,
+      };
+    default:
+      return state;
+  }
+};
